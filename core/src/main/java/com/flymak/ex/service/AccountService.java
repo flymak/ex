046dceafc6a5c6540da79ba149b2628bb2dbd286
Peng Liu <findliupeng@gmail.com>
2020-07-22 17:47:23 +0800

    add modle

--- core/src/main/java/com/flymak/ex/service/AccountService.java
+++ core/src/main/java/com/flymak/ex/service/AccountService.java
@@ -1,71 +1,51 @@
 package com.flymak.ex.service;
 
 import com.flymak.ex.domain.Account;
-import com.flymak.ex.domain.AccountType;
+import com.flymak.ex.domain.AccountFlow;
 import com.flymak.ex.exception.AccountNoSufficientBalance;
 import com.flymak.ex.exception.AccountNotFound;
+import com.flymak.ex.repository.AccountFlowRepository;
 import com.flymak.ex.repository.AccountRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.math.BigDecimal;
 
 @Service
 public class AccountService {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     private AccountRepository repository;
+
+    private AccountFlowRepository flowRepository;
 
     @Value("{ex.systemUser}")
     private long systemUserId;
 
-
     @Autowired
-    public AccountService(AccountRepository repository) {
+    public AccountService(AccountRepository repository, AccountFlowRepository flowRepository) {
         this.repository = repository;
+        this.flowRepository = flowRepository;
     }
 
     @Transactional
-    public void deposit(long userId, String currency, BigDecimal amount) {
-        String systemAccountNo = Account.accountNo(systemUserId, currency, AccountType.AVAILABLE);
-        String toAccountNo = Account.accountNo(userId, currency, AccountType.AVAILABLE);
-        transfer(systemAccountNo, toAccountNo, amount, false);
-    }
+    void transfer(AccountFlow flow, boolean checkBalance) {
+        logger.info("start transfer checkBalance {}, {}...", checkBalance, flow);
+        Account from = repository.findByAccountNo(flow.getFromAccountNo()).orElseThrow(() -> new AccountNotFound(flow.getFromAccountNo()));
+        Account to = repository.findByAccountNo(flow.getToAccountNo()).orElseThrow(() -> new AccountNotFound(flow.getToAccountNo()));
 
-    @Transactional
-    public void withdraw(long userId, String currency, BigDecimal amount) {
-        String fromAccountNo = Account.accountNo(userId, currency, AccountType.AVAILABLE);
-        String toAccountNo = Account.accountNo(userId, currency, AccountType.FROZEN);
-        transfer(fromAccountNo, toAccountNo, amount, true);
-    }
-
-    @Transactional
-    public void withdrawComplete(long userId, String currency, BigDecimal amount) {
-        String fromAccountNo = Account.accountNo(userId, currency, AccountType.FROZEN);
-        String toAccountNo = Account.accountNo(systemUserId, currency, AccountType.AVAILABLE);
-        transfer(fromAccountNo, toAccountNo, amount, true);
-    }
-
-    @Transactional
-    public void order(long userId, String currency, BigDecimal amount) {
-        String fromAccountNo = Account.accountNo(userId, currency, AccountType.AVAILABLE);
-        String toAccountNo = Account.accountNo(userId, currency, AccountType.FROZEN);
-        transfer(fromAccountNo, toAccountNo, amount, true);
-    }
-
-    @Transactional
-    public void transfer(String fromAccountNo, String toAccountNo, BigDecimal amount, boolean checkBalance) {
-        Account from = repository.findByAccountNo(fromAccountNo).orElseThrow(() -> new AccountNotFound(fromAccountNo));
-        Account to = repository.findByAccountNo(toAccountNo).orElseThrow(() -> new AccountNotFound(toAccountNo));
-
-        if (checkBalance && from.getBalance().compareTo(amount) < 0) {
-            throw new AccountNoSufficientBalance(fromAccountNo, amount);
+        if (checkBalance && from.getBalance().compareTo(flow.getAmount()) < 0) {
+            throw new AccountNoSufficientBalance(flow.getFromAccountNo(), flow.getAmount());
         }
 
-        from.setBalance(from.getBalance().subtract(amount));
-        to.setBalance(to.getBalance().add(amount));
-
+        from.setBalance(from.getBalance().subtract(flow.getAmount()));
+        to.setBalance(to.getBalance().add(flow.getAmount()));
         repository.save(from);
         repository.save(to);
+        flowRepository.save(flow);
+        logger.info("finish transfer checkBalance {}, {}", checkBalance, flow);
     }
 }
